4d8a7dbb2c7d1d25ab57e3da8dd8ff573144b245
mespinoza30 <m.espinoza16@hotmail.com>
2021-03-11 11:58:47 -0800

    Save button added to Pet Cards

--- src/components/PetApi.js
+++ src/components/PetApi.js
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab'
+import FavoriteIcon from '@material-ui/icons/Favorite'
 
 const { REACT_APP_SERVER_URL } = process.env;
+
+
 
 function PetApi() {
 
@@ -25,30 +29,38 @@ function PetApi() {
   }, [])
   const listOfPets = petApi.map((petItem, index) => {
 
+
     return (
 
-
-      <Card className="animalCard">
-        <CardContent>
-          <div key={index}>
-            {petItem.photos[0] ? <img src={petItem.photos[0].medium} /> : <p>no photo</p>}
-            <Typography variant="h5"><p className="petName">Name: {petItem.name}</p></Typography>
-
-            {/* <img src={petItem.photos.medium} /> */}
-            {/* <p>Type: {petItem.type}</p> */}
-            <Typography variant="body2" color="textSecondary" component="p">
-              <p><b>Species:</b> {petItem.species}   <b>Gender:</b> {petItem.gender}   <b>Age:</b> {petItem.age}</p>
-              <p><b>Email:</b> {(petItem.contact[0].email)}</p>
-              <p><b>Phone Number:</b> {(petItem.contact[0].phone)}</p>
-              <p><b>City:</b> {(petItem.contact[0].address.city)}   <b>State:</b> {(petItem.contact[0].address.state)}   <b>Zip Code:</b> {(petItem.contact[0].address.postcode)}</p>
-              <p><b>Country:</b> {(petItem.contact[0].address.country)}</p>
-            </Typography>
-          </div>
-        </CardContent>  
-        </Card>
-    )  
-  })
   
+        <Card className="animalCard">            
+        <Fab size="small" color="secondary" aria-label="like" className="saveButton">
+          <FavoriteIcon />
+        </Fab>
+          <CardContent>
+            <div key={index}>
+
+              {petItem.photos[0] ? <img src={petItem.photos[0].medium} /> : <p>no photo</p>}
+
+              <Typography variant="h5"><p className="petName">Name: {petItem.name}</p></Typography>
+
+              {/* <img src={petItem.photos.medium} /> */}
+              {/* <p>Type: {petItem.type}</p> */}
+              <Typography variant="body2" color="textSecondary" component="p">
+                <p><b>Species:</b> {petItem.species}   <b>Gender:</b> {petItem.gender}   <b>Age:</b> {petItem.age}</p>
+                <p><b>Email:</b> {(petItem.contact[0].email)}</p>
+                <p><b>Phone Number:</b> {(petItem.contact[0].phone)}</p>
+                <p><b>City:</b> {(petItem.contact[0].address.city)}   <b>State:</b> {(petItem.contact[0].address.state)}   <b>Zip Code:</b> {(petItem.contact[0].address.postcode)}</p>
+                <p><b>Country:</b> {(petItem.contact[0].address.country)}</p>
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
+
+
+    )
+  })
+
   return (
     <div className="animalCard">
       {listOfPets}
