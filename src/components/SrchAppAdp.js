aab24d7cc69f45e2184ef223fecfa0cb87fd145b
mespinoza30 <m.espinoza16@hotmail.com>
2021-03-11 09:30:39 -0800

    Added welcome text

--- src/components/SrchAppAdp.js
+++ src/components/SrchAppAdp.js
@@ -4,26 +4,27 @@ import React from 'react';
 
 const SrchAppAdp = () => {
     return (
-        <div class="container text-center">
-            <div>
-                <p className="headerSrchAdpt">Bring your new best friend home today!</p>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-6">
-                    <p className="srchAdptTxt">Search</p>
-                    <p className="description">Search for your new cat or dog best friend near you.</p>
+        <div className="longBox">
+            <div class="container text-center">
+                <div>
+                    <p className="headerSrchAdpt">Bring your new best friend home today!</p>
                 </div>
 
-                <div class="col-sm-6">
-                    <p className="srchAdptTxt">Adopt</p>
-                    <p className="description">Adopting has never been easier. Just search and apply!</p>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p className="srchAdptTxt">Search</p>
+                        <p className="description">Search for your new cat or dog best friend near you.</p>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <p className="srchAdptTxt">Adopt</p>
+                        <p className="description">Adopting has never been easier. Just search and apply!</p>
+                    </div>
+
                 </div>
+                <br></br>
 
             </div>
-            <br></br>
-            <br></br>
-            <br></br>
         </div>
     );
 }
