985196a649ca46d793ad3328036be6e2d8d0b328
mespinoza30 <m.espinoza16@hotmail.com>
2021-03-11 17:15:49 -0800

    Changed header/footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer bg-dark">
+        <footer className="footer">
             <div className="container text-center">
-                <span className="text-muted">@2021 Built by Diego & Marisela</span>
+                <span className="textFooter">@2021 Built by Diego Espinoza & Marisela Espinoza 🐾</span>
             </div>
         </footer>
     )
