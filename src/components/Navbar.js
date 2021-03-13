c503ad6d235c84b99defff9cb4d42b6c2a02d335
digeoesp <diegocrypto@outlook.com>
2021-03-12 21:40:48 -0800

    erased about route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,9 +16,7 @@ const Navbar = (props) => {
                         <li className="nav-item">
                             <NavLink className="nav-link" exact to="/">Home</NavLink>
                         </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link"  to="/about">About</NavLink>
-                        </li>
+                       
                         <li className="nav-item">
                             <NavLink className="nav-link"  to="/pets">Pets</NavLink>
                         </li>
