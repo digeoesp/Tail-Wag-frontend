f5bd92cca8780e11b1f6602ab632d42a291d07e6
mespinoza30 <m.espinoza16@hotmail.com>
2021-03-12 22:06:31 -0800

    CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,11 @@ import React from 'react';
 const Footer = () => {
     
     return (
-        
         <footer className="footer">
             <div className="container text-center">
                 <span className="textFooter">@2021 Built by Diego Espinoza & Marisela Espinoza</span>
             </div>
         </footer>
-        
     )
 }
 
